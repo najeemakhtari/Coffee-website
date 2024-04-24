21023616e0cdf82498152b0e76f11040613d54fe
najeemakhtari <mohammadnajeem1996@gmail.com>
2024-04-24 22:17:08 +0330

    varint added to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,5 +31,11 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    function({addVariant}){
+      addVariant('child','& > *');
+      addVariant('child-hover', '& > *:hover')
+
+    }
+  ],
 };
