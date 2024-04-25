b414805051691cf449c870acec6b2efcd810989c
najeemakhtari <mohammadnajeem1996@gmail.com>
2024-04-25 13:42:26 +0330

    Theme toggle button developed

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,13 @@
 "use strict";
+
+const toggleThemeBtn = document.querySelector('#toggle-theme')
+
+toggleThemeBtn.addEventListener('click',()=>{
+    if(localStorage.theme =='dark'){
+        document.documentElement.classList.remove('dark')
+        localStorage.theme = 'light'
+    }else{
+        document.documentElement.classList.add('dark');
+        localStorage.setItem('theme', 'dark')
+    }
+})
